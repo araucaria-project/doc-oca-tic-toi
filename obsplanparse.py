035c9a2f8b4084d79f48872e1c2a69fe8ae3a523
Zuzanna Jasinska <zuzanna.jasinska07@gmail.com>
2022-09-21 12:45:33 +0200

    1.0

--- obsplanparse.py
+++ obsplanparse.py
@@ -1,96 +1,107 @@
+import copy
+
 from lark import Lark, Visitor
 
-kwargs_dict = dict()
-args_list = []
-command_str = ''
-commands_list = []
 
 line_grammar = r"""
   ?start: commands
-    
-    !commands   : command (args* kwargs*)*  
-    !command    : word
-    kwargs      : kwarg*
-    args        : arg*
-    arg        : val
-    kwarg      : kw "=" val*
-    
-    !kw         : string_simple
-    !val        : string_simple | string_quoted
-    ?string_quoted  : /[\"].*[\"]|[\'].*[\']/
-    ?word           : WORD
-    ?string_simple  : /[^\s,^=]+/
-    ?number         : SIGNED_NUMBER
-    
-    %import common.WORD
-    %import common.SIGNED_NUMBER
-    %import common.WS
-    
-    %ignore WS
-    %ignore /,/
+
+!commands   : command (args* kwargs*)* 
+!command    : word 
+kwargs      : kwarg* 
+args        : arg* 
+arg         : val 
+kwarg       : kw "=" val* 
+
+!kw             : string_simple
+!val            : string_simple | string_quoted
+?string_quoted  : /[\"].*[\"]|[\'].*[\']/
+?word           : WORD
+?string_simple  : /[^\s,^=]+/
+?number         : SIGNED_NUMBER
+
+%import common.WORD
+%import common.SIGNED_NUMBER
+%import common.WS
+
+%ignore WS
+%ignore /,/
+%ignore /#.*/
   """
 
+
 class ConvertElementsOfDict(Visitor):
-    # converts kwargs to dictionary
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.kwargs_dict = {}
+        self.args_list = []
+        self.command_str = ''
+
     def kwarg(self, tree):
+        """converts kwargs to dictionary"""
         assert tree.data == "kwarg"
         val_list = []
 
         # tree.children[i].data -> kw, val1, val2, ...
         kw = ''+tree.children[0].children[0].value  # value of kw (str)
 
+        # for child  in tree.children:
+        #     val_list.append('' + child.children[0].value)
+
         i = 1
-        while (1):  # making list of vals in kw
+        while True:  # make list of vals in kw
             try:
                 val_list.append(''+tree.children[i].children[0].value)
             except:
                 break
             i = i + 1
 
-        global kwargs_dict
-        kwargs_dict[kw] = val_list
+        self.kwargs_dict[kw] = val_list
 
     # converts args to list
     def arg(self, tree):
         assert tree.data == "arg"
-        global args_list
-        args_list.append('' + tree.children[0].children[0].value)
+        self.args_list.append('' + tree.children[0].children[0].value)
 
     def command(self, tree):
         assert tree.data == "command"
-        global command_str
-        command_str = tree.children[0].value
+        self.command_str = tree.children[0].value
 
-def convertToDict(dictionary):
-    dictionary["command"] = command_str
-    dictionary["args"] = args_list
-    dictionary["kwargs"] = kwargs_dict
-    return dictionary
+
+# converts one command (line)
+def convertToDict(visitor):
+    ret_dict = {
+        "command": visitor.command_str,
+        "args": visitor.args_list,
+        "kwargs": visitor.kwargs_dict
+    }
+    return ret_dict
+
 
 def readFileAndConvert(file_name):
-    temp_dict = {
-        "command": "",
-        "args": "",
-        "kwargs": ""
-    }
     dictionary = dict()
+    commands_list = []
 
     line_parser = Lark(line_grammar)
     parse = line_parser.parse
 
-    file = open(file_name, "r")
-    line = file.readline()
+    with open(file_name, "r") as file:
+        for line in file:
 
-    while line:
-        tree = parse(line)
-        ConvertElementsOfDict().visit(tree)
-        temp_dict = convertToDict(temp_dict)
-        commands_list.append(temp_dict)
-        line = file.readline()
-    file.close()
+            tree = parse(line)
+            visitor = ConvertElementsOfDict()
+            visitor.visit(tree)
+
+            command_dict = convertToDict(visitor)
+
+            commands_list.append(command_dict)
+
+
 
     dictionary["commands"] = commands_list
     return dictionary
+
 
 # writes dictionary to file
 def writeDictToFile(file_name, dictionary):
@@ -100,5 +111,5 @@ def writeDictToFile(file_name, dictionary):
 
 
 if __name__ == '__main__':
-    dictionary = readFileAndConvert("file.txt")
+    dictionary = readFileAndConvert("tests/file3.txt")
     writeDictToFile("dictionary.txt", dictionary)
---
+++ tests/test_parser.py
@@ -0,0 +1,68 @@
+import unittest
+
+from obsplanparse import readFileAndConvert, writeDictToFile
+
+
+class ParserCase(unittest.TestCase):
+    def test_parsing_file1(self):
+        result = readFileAndConvert('file1.txt')
+        expected = {'commands': [
+            {'command': 'ZERO', 'args': [], 'kwargs': {'seq': ['15/I/0']}},
+            {'command': 'DARK', 'args': [], 'kwargs': {'seq': ['10/V/300', '10/I/200']}},
+            {'command': 'DOMEFLAT', 'args': [], 'kwargs': {'seq': ['7/V/20', '7/I/20']}},
+            {'command': 'DOMEFLAT', 'args': [], 'kwargs': {'seq': ['10/str_u/100'], 'domeflat_lamp': ['0.7']}}]}
+        self.assertDictEqual(result, expected)
+
+    def test_parsing_file2(self):
+        result = readFileAndConvert('file2.txt')
+        expected = {'commands': [
+            {'command': 'BEGINSEQUENCE', 'args': [], 'kwargs': {'execute_at_dawn': ['-6'], 'priority': ['+10']}},
+            {'command': 'SKYFLAT', 'args': [], 'kwargs': {'alt': ['60:00:00'], 'az': ['270:00:00'], 'seq': ['10/I/20', '10/V/30']}},
+            {'command': 'SKYFLAT', 'args': [], 'kwargs': {'seq': ['10/I/0', '10/V/0'], 'skyflat_adu': ['30']}},
+            {'command': 'SKYFLAT', 'args': [], 'kwargs': {'alt': ['60:00:00'], 'az': ['270:00:00'], 'seq': ['10/I/20', '10/V/30']}},
+            {'command': 'SKYFLAT', 'args': [], 'kwargs': {'seq': ['10/I/0', '10/V/0'], 'skyflat_adu': ['30']}},
+            {'command': 'ENDSEQUENCE', 'args': [], 'kwargs': {}}]}
+        self.assertDictEqual(result, expected)
+
+    def test_parsing_file3(self):
+        result = readFileAndConvert('file3.txt')
+        expected = {'commands': [
+            {'command': 'BEGINSEQUENCE', 'args': [], 'kwargs': {'execute_at_time': ['02:21:43'], 'priority': ['+30']}},
+            {'command': 'OBJECT', 'args': ['FF_Aql', '18:58:14.75', '17:21:39.29'], 'kwargs': {'seq': ['2/I/60', '2/V/70']}},
+            {'command': 'ENDSEQUENCE', 'args': [], 'kwargs': {}}]}
+        self.assertDictEqual(result, expected)
+
+    def test_parsing_file3_with_sequence(self):
+        result = readFileAndConvert('file3.txt')
+        expected = {'commands': [
+            {
+                'command': 'SEQUENCE',
+                'args': [],
+                'kwargs': {'execute_at_time': ['02:21:43'], 'priority': ['+30']},
+                'commands': [
+                    {
+                        'command': 'OBJECT',
+                        'args': ['FF_Aql', '18:58:14.75', '17:21:39.29'],
+                        'kwargs': {'seq': ['2/I/60', '2/V/70']}
+                    },
+                ]
+             }
+            ]}
+
+        self.assertDictEqual(result, expected)
+
+    def test_writing_to_file(self):
+        dictionary = {'word1': 'hello', 'word2': 'world', 'word3': '!'}
+        writeDictToFile("test_file.txt", dictionary)
+
+        file = open("test_file.txt")
+        result = file.readlines()   # should return list of file lines
+        file.close()
+
+        expected = ["{'word1': 'hello', 'word2': 'world', 'word3': '!'}"]
+
+        self.assertListEqual(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
